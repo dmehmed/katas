d17146b607597acefdf61929c45ccf9985eee9a3
Deniz A. Mehmed <denizmehmed97@gmail.com>
2021-11-30 12:09:36 +0200

    Init FizzBuzz kata

---
+++ FizzBuzz/FizzBuzz/FizzBuzz.h
@@ -0,0 +1,18 @@
+//
+//  FizzBuzz.h
+//  FizzBuzz
+//
+//  Created by Deniz Mehmed on 30.11.21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FizzBuzz.
+FOUNDATION_EXPORT double FizzBuzzVersionNumber;
+
+//! Project version string for FizzBuzz.
+FOUNDATION_EXPORT const unsigned char FizzBuzzVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FizzBuzz/PublicHeader.h>
+
+
