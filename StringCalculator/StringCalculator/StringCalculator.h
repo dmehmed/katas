c964679664b99dce2c5354a1b64db634dc0fe9d1
Deniz A. Mehmed <denizmehmed97@gmail.com>
2021-12-01 16:00:57 +0200

    New project String calculator

---
+++ StringCalculator/StringCalculator/StringCalculator.h
@@ -0,0 +1,18 @@
+//
+//  StringCalculator.h
+//  StringCalculator
+//
+//  Created by Deniz Mehmed on 1.12.21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for StringCalculator.
+FOUNDATION_EXPORT double StringCalculatorVersionNumber;
+
+//! Project version string for StringCalculator.
+FOUNDATION_EXPORT const unsigned char StringCalculatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StringCalculator/PublicHeader.h>
+
+
---
+++ StringCalculator/StringCalculator/StringCalculator.swift
@@ -0,0 +1,12 @@
+//
+//  StringCalculator.swift
+//  StringCalculator
+//
+//  Created by Deniz Mehmed on 1.12.21.
+//
+
+import Foundation
+
+class StringCalculator {
+    
+}
