cd6909be230510b7381044a29ef0bb35e9dee1e0
Deniz A. Mehmed <denizmehmed97@gmail.com>
2021-12-02 16:15:43 +0200

    Test multiple longer length separators

--- StringCalculator/StringCalculatorTests/StringCalculatorTests.swift
+++ StringCalculator/StringCalculatorTests/StringCalculatorTests.swift
@@ -68,6 +68,10 @@ class StringCalculatorTests: XCTestCase {
         verify("//[*][%]\n1*2%3", result: 6)
     }
     
+    func testAddNumbersWithMultipleLongerLengthSeparators() {
+        verify("//[foo][bar]\n1foo2bar3", result: 6)
+    }
+    
     private func verify(
         _ input: String,
         result: Int,
