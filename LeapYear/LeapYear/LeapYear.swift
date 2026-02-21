83e2b27edaefa828d9b54a184b193995d4d76bde
Deniz Mehmed <denizmehmed97@gmail.com>
2026-02-21 18:31:28 +0100

    Test connection

--- LeapYear/LeapYear/LeapYear.swift
+++ LeapYear/LeapYear/LeapYear.swift
@@ -8,19 +8,16 @@
 import Foundation
 
 class LeapYear {
-    
     func isLeapYear(year: Int) -> Bool {
         return year.isDivisible(by: 400) || ( year.isDivisible(by: 4) && !year.isDivisible(by: 100) )
     }
-    
 }
 
 
 // MARK: - Int extension
+
 extension Int {
-    
     func isDivisible(by number: Int) -> Bool {
         self % number == 0
     }
-    
 }
